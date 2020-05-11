642fc7cf4e40e53f25fdc2a20ee08bb14a4bc4cc
Amana Ammishaddai <amana.ammi@gmail.com>
2020-05-11 04:40:00 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -4,7 +4,7 @@ def my_all?(collection)
   i = 0
   new_array = []
   while i < collection.length
-  new_array << yield(collection{i}) 
+  new_array << yield(collection[i]) 
   i += 1
   end
 if new_array.include?(false)
@@ -13,3 +13,4 @@ else
   true
   end
 end
+
