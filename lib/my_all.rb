8ff5b4276124f3c1ad0082a305529e938dd15174
Amana Ammishaddai <amana.ammi@gmail.com>
2020-05-11 04:35:11 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -2,9 +2,10 @@ require 'pry'
 
 def my_all?(collection)
   i = 0
+  new_array = []
   while i < collection.length
-  yield(collection{i})
+  yield(collection{i}) << new_array
   i += 1
   end
-
+new_array
 end
